7fe1ba6be0164b993c55b8d93b518fa2e71b076a
Jason Kincl <kincljc@ornl.gov>
2019-12-31 09:26:28 -0500

    Adding log message for temporary network error

--- agent/agent.go
+++ agent/agent.go
@@ -1,9 +1,11 @@
 package agent
 
 import (
+	"log"
+	"net"
+
 	"github.com/negasus/haproxy-spoe-go/request"
 	"github.com/negasus/haproxy-spoe-go/worker"
-	"net"
 )
 
 func New(handler func(*request.Request)) *Agent {
@@ -23,6 +25,7 @@ func (agent *Agent) Serve(listener net.Listener) error {
 		conn, err := listener.Accept()
 		if err != nil {
 			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+				log.Println("Unknown temporary net error")
 				continue
 			}
 			return err
